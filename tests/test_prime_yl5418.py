bc88fd3a0be04e9c21ccf7dfa2ae8c76b16684cf
Yixin Liu <lyxjack2014@gmail.com>
2023-10-27 15:56:00 -0400

    hw_07 Q3 Commit
    pytest successful.

--- tests/test_prime_yl5418.py
+++ tests/test_prime_yl5418.py
@@ -1,1 +1,12 @@
-from prime_yl5418 import prime_yl5418
+import pytest
+
+from prime_yl5418.prime_yl5418 import is_prime
+
+
+def test_is_prime():
+    assert is_prime(2), 'Error: your input is a prime number'
+    assert is_prime(7), 'Error: your input is a prime number'
+    
+    # Test using a composite numbers (e.g. 8, 9)
+    assert not is_prime(8), 'Error: your input is not a prime number'
+    assert not is_prime(9), 'Error: your input is not a prime number'
